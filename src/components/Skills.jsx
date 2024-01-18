4f9d1564e910e3c8b0d289e04aea6835ca71dbe2
kyota-hoshi <k-hoshi@kyotakotech.blog>
2024-01-18 10:21:30 +0900

    サイトの全体的な見た目を調節

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,33 +8,33 @@ export const Skills = () => {
 
   return (
     <div id="skills">
-      <div className="container">
-        <div className="heading">
-          <h2>Skills</h2>
-        </div>
-        <div className="skills-container">
-          {
-            fetchRequestState === requestStates.loading && (
-              <p className="description">取得中...</p>
-            )
-          }
-          {
-            fetchRequestState === requestStates.error && (
-              <p className="description">エラーが発生しました</p>
-            )
-          }
-          {
-            fetchRequestState === requestStates.success && (
-              sortedLanguageList().map((item, index) => (
-                <div className="skill-item" key={index}>
-                  <p className="description"><strong>{item.language}</strong></p>
-                  <CircularProgressbar value={convertCountToPercentage(item.count)} text={`${convertCountToPercentage(item.count)}%`} />
-                </div>
-              ))
-            )
-          }
-        </div>
+      {/* <div className="container"> */}
+      <div className="heading">
+        <h2>Skills</h2>
       </div>
+      <div className="skills-container">
+        {
+          fetchRequestState === requestStates.loading && (
+            <p className="description">取得中...</p>
+          )
+        }
+        {
+          fetchRequestState === requestStates.error && (
+            <p className="description">エラーが発生しました</p>
+          )
+        }
+        {
+          fetchRequestState === requestStates.success && (
+            sortedLanguageList().map((item, index) => (
+              <div className="skill-item" key={index}>
+                <p className="description"><strong>{item.language}</strong></p>
+                <CircularProgressbar value={convertCountToPercentage(item.count)} text={`${convertCountToPercentage(item.count)}%`} />
+              </div>
+            ))
+          )
+        }
+      </div>
+      {/* </div> */}
     </div>
   );
 };
